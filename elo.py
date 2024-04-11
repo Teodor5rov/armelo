7f80563aa118c15a67b42d248380601cbcf845bd
Teodor <teo@bulpost.com>
2024-04-12 00:51:52 +0200

    init matches

--- elo.py
+++ elo.py
@@ -1,22 +1,19 @@
-K = 30
-
-
 def expected_score(armwrestler_a_elo, armwrestler_b_elo):
-    expected_a_elo = 1 / (1 + pow(10, ((armwrestler_b_elo - armwrestler_a_elo) / 400)))
+    expected_a_elo = 1 / (1 + pow(10, ((armwrestler_b_elo - armwrestler_a_elo) / 500)))
     expected_b_elo = 1 - expected_a_elo
 
     return expected_a_elo, expected_b_elo
 
 
-def calculate_elo(armwrestler_a_elo, armwrestler_b_elo, actual_score):
+def calculate_elo(armwrestler_a_elo, armwrestler_b_elo, actual_score, k = 100):
     total_rounds = sum(actual_score)
     actual_a_elo = actual_score[0] / total_rounds
     actual_b_elo = actual_score[1] / total_rounds
     
     expected_a_elo, expected_b_elo = expected_score(armwrestler_a_elo, armwrestler_b_elo)
 
-    updated_a_elo = round(armwrestler_a_elo + K * (actual_a_elo - expected_a_elo))
-    updated_b_elo = round(armwrestler_b_elo + K * (actual_b_elo - expected_b_elo))
+    updated_a_elo = round(armwrestler_a_elo + k * (actual_a_elo - expected_a_elo))
+    updated_b_elo = round(armwrestler_b_elo + k * (actual_b_elo - expected_b_elo))
 
     return updated_a_elo, updated_b_elo
 
---
+++ init_matches/names.py
@@ -0,0 +1,17 @@
+import csv
+from itertools import permutations
+
+with open('names.csv', 'r', newline='', encoding='utf-8') as csvfile:
+    reader = csv.reader(csvfile)
+    next(reader) 
+    names = [' '.join(row) for row in reader]  
+
+pairs = list(permutations(names, 2))
+
+with open('output.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(['Armwrestler1', 'Armwrestler2', 'Score1', 'Score2'])
+    for pair in pairs:
+        writer.writerow([pair[0], pair[1], '', ''])
+
+print(f'Output CSV with {len(pairs)} matches has been created.')
